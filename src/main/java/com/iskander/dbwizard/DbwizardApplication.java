f42e249a6b534251c850f1d87c42259c5964f227
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2025-02-14 16:56:32 +0500

    init commit

---
+++ src/main/java/com/iskander/dbwizard/DbwizardApplication.java
@@ -0,0 +1,13 @@
+package com.iskander.dbwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbwizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbwizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iskander/dbwizard/DbwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskander.dbwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbwizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
